20cb4c70e373c971fd0f6fbbcbe1a733b7ec5ecc
Jim <jimlotruglio@yahoo.com>
2020-01-09 18:10:52 -0700

    first commit

---
+++ lib/song.rb
@@ -0,0 +1,52 @@
+class Song
+  attr_accessor :name, :artist
+  attr_reader :id
+
+  def initialize(id = nil, name, artist)
+    @name = name
+    @artist = artist
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        artist TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE songs
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO songs(name, artist) VALUES (?,?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.artist)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+  end
+
+  def self.create(name:, artist:)
+    song = Song.new(name, artist)
+    song.save
+    song
+  end
+
+  def self.table_name
+    self.to_s.downcase + "s"
+  end
+
+
+
+
+end
---
+++ spec/song_spec.rb
@@ -0,0 +1,84 @@
+require "spec_helper"
+require 'pry'
+#require_relative "../config/environment.rb"
+
+
+describe "Song" do
+  let(:longview) {Song.new("Longview", "Green Day")}
+
+  before(:each) do
+    DB[:conn].execute("DROP TABLE IF EXISTS songs")
+  end
+
+  describe "When initialized with a name and artist" do
+    it 'the artist attribute can be accessed' do
+      song = Song.new("DumpWeed", "Blink 182")
+      expect(song.artist).to eq("Blink 182")
+    end
+
+    it "name attribute can be accessed" do
+      song = Song.new("Green Day", "American Idiot")
+      expect(song.artist).to eq("American Idiot")
+    end
+  end
+
+  it "responds to a getter for :id" do
+    expect(longview).to respond_to(:id)
+  end
+
+  it "does not provide a setter for :id" do
+    expect{longview.id = 1}.to raise_error(NoMethodError)
+  end
+
+  describe ".create_table" do
+    it 'creates table for songs' do
+      Song.create_table
+      table_check_sql = "SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name='songs';"
+      expect(DB[:conn].execute(table_check_sql)[0]).to eql(['songs'])
+    end
+  end
+
+    describe ".drop_table" do
+      it 'drops table for songs' do
+        Song.create_table
+        Song.drop_table
+
+      sql = "SELECT tbl_name FROM sqlite_master WHERE type = 'table' AND tbl_name = 'songs';"
+      expect(DB[:conn].execute(sql)[0]).to eq(nil)
+    end
+  end
+
+  describe "#save" do
+    it 'saves an instance of song class to the database' do
+      Song.create_table
+      longview.save
+      expect(longview.id).to eq(1)
+
+      expect(DB[:conn].execute("SELECT * FROM songs")).to eq([[1,"Longview", "Green Day"]])
+    end
+  end
+
+  describe ".create" do
+    before(:each) do
+      Song.create_table
+    end
+    it 'takes a hash of attributes and creates new song object, uses save to save it to song table' do
+      Song.create(name: "Dumpweed", artist: "Blink 182")
+      expect(DB[:conn].execute("SELECT * FROM songs")).to eq([[1, "Dumpweed", "Blink 182"]])
+    end
+    it 'returns the new object' do
+      song = Song.create(name: "Hey Jude", artist: "Beatles")
+      expect(song).to be_a(Song)
+      expect(song.artist).to eq("Beatles")
+      expect(song.name).to eq("Hey Jude")
+      expect(song.id).to eq(1)
+    end
+  end
+
+  describe ".table_name" do
+    it "returns the table name" do
+      table = Song.table_name
+      expect(table).to eq("songs")
+    end
+  end 
+end
